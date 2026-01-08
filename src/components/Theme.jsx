ccb2124471ec18a1bcd83356980110d1e344f4ba
abhijithpr08 <abhith628@gmail.com>
2026-01-08 16:45:48 +0530

    Nested context consumption

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -1,24 +1,34 @@
 import React, { useContext } from 'react'
 
 const ThemeContext = React.createContext("light")
+const UserContext = React.createContext("Guest")
 
-// const Toolbar =()=>{
-//   const theme = useContext(ThemeContext) 
-//   return <div>Theme : {theme}</div>
+const Toolbar = () => {
+  const theme = useContext(ThemeContext);
+  const user = useContext(UserContext);
+
+  return (
+    <div>
+      Theme: {theme} <br />
+      User: {user}
+    </div>
+  );
+};
+
+// class Toolbar extends React.Component {
+//   static contextType = ThemeContext;
+
+//   render() {
+//     return <div>Theme: {this.context}</div>;
+//   }
 // }
-
-class Toolbar extends React.Component {
-  static contextType = ThemeContext;
-
-  render() {
-    return <div>Theme: {this.context}</div>;
-  }
-}
 
 const Theme = () => {
   return (
-    <ThemeContext.Provider value='dark'>
-      <Toolbar/>
+    <ThemeContext.Provider value="dark">
+      <UserContext.Provider value="Abhijith">
+        <Toolbar />
+      </UserContext.Provider>
     </ThemeContext.Provider>
   )
 }
