4167ce0b9ec3d06b1d43479b2699c8253ab515fe
abhijithpr08 <abhith628@gmail.com>
2026-01-08 16:34:15 +0530

    create theme context

---
+++ src/components/Theme.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ThemeContext = React.createContext("light")
+const Theme = () => {
+  return (
+    <div>Theme Context Created</div>
+  )
+}
+
+export default Theme
