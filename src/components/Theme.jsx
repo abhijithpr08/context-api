94e86a9308f2c10670577e04d8d19215aac838ed
abhijithpr08 <abhith628@gmail.com>
2026-01-08 16:42:50 +0530

    Class component context consumption

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -2,9 +2,17 @@ import React, { useContext } from 'react'
 
 const ThemeContext = React.createContext("light")
 
-const Toolbar =()=>{
-  const theme = useContext(ThemeContext) 
-  return <div>Theme : {theme}</div>
+// const Toolbar =()=>{
+//   const theme = useContext(ThemeContext) 
+//   return <div>Theme : {theme}</div>
+// }
+
+class Toolbar extends React.Component {
+  static contextType = ThemeContext;
+
+  render() {
+    return <div>Theme: {this.context}</div>;
+  }
 }
 
 const Theme = () => {
